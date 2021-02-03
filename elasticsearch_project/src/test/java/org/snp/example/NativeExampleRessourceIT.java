a3c96e255a2e1f0646298ba413503bc0fe046c8d
NidhalWers <nteyeb75@gmail.com>
2021-02-03 19:18:17 +0100

    initial commit

---
+++ elasticsearch_project/src/test/java/org/snp/example/NativeExampleRessourceIT.java
@@ -0,0 +1,9 @@
+package test.java.org.snp.example;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleRessourceIT extends ExampleRessourceTest {
+
+    // Execute the same tests but in native mode.
+}
