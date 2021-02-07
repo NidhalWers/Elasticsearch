365e198d79af6a47ffd4f2831e6e8810206df7cb
NidhalWers <nteyeb75@gmail.com>
2021-02-07 14:46:46 +0100

    index key

--- elasticsearch_project/src/main/java/org/snp/indexage/entities/Index.java
+++ elasticsearch_project/src/main/java/org/snp/indexage/entities/Index.java
@@ -1,7 +1,5 @@
 package org.snp.indexage.entities;
 
-import org.snp.utils.StringUtils;
-
 import java.util.*;
 
 public class Index {
@@ -9,7 +7,10 @@ public class Index {
     private List<Column> columns = new ArrayList<>();
 
     public void insert(String value, String ...keys ){
-        String key = StringUtils.getKeyFromTab(keys);
+        String key = "";
+        for(String k : keys ){
+            key+=k;
+        }
         if (index.get(key)==null){
             index.put(key,new ArrayList<String>());
             index.get(key).add(value);
