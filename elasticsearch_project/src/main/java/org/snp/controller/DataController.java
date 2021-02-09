0e00a352e228aeeb9ea89b0a68d262b8b6890262
pauliusmi20 <pauliusmick@yahoo.com>
2021-02-09 20:27:04 +0100

    added bad request

--- elasticsearch_project/src/main/java/org/snp/controller/DataController.java
+++ elasticsearch_project/src/main/java/org/snp/controller/DataController.java
@@ -3,14 +3,13 @@ package org.snp.controller;
 import org.snp.model.communication.Message;
 import org.snp.model.communication.MessageAttachment;
 import org.snp.model.credentials.DataCredentials;
-import org.snp.model.credentials.IndexCredentials;
 import org.snp.service.TableService;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("/elasticsearch/data")
+@Path("/data")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class DataController {
@@ -20,7 +19,7 @@ public class DataController {
     @POST
     @Path("/load/")
     public String loadData(DataCredentials dataCredentials){
-        Message message = tableService.addLign(dataCredentials);
+        Message message = tableService.addLine(dataCredentials);
         if(message.hasAttachment())
             return ((MessageAttachment)message).getAttachment().toString();
         else
@@ -31,7 +30,7 @@ public class DataController {
 
 
     @GET
-    @Path("/get/")
+    @Path("/get")
     public String getLigns(DataCredentials dataCredentials){
         Message message = tableService.query(dataCredentials);
         if(message.hasAttachment())
