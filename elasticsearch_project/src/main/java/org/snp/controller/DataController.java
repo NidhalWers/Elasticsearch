822d355ce68d8113db2f6f9201029545dfccd05b
pauliusmi20 <pauliusmick@yahoo.com>
2021-03-09 17:24:26 +0100

    added post multipart csv

--- elasticsearch_project/src/main/java/org/snp/controller/DataController.java
+++ elasticsearch_project/src/main/java/org/snp/controller/DataController.java
@@ -1,36 +1,51 @@
 package org.snp.controller;
 
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 import org.snp.model.communication.Message;
 import org.snp.model.communication.MessageAttachment;
 import org.snp.model.credentials.DataCredentials;
+import org.snp.model.multipart.MultipartBody;
 import org.snp.service.DataService;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static io.netty.util.internal.StringUtil.COMMA;
 
 @Path("/data")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
 public class DataController {
 
     @Inject private DataService dataService;
 
     @POST
     @Path("/load/")
-    public String loadData(DataCredentials dataCredentials){
-        Message message = dataService.load(dataCredentials);
-        if(message.hasAttachment())
-            return ((MessageAttachment)message).getAttachment().toString();
-        else
-            throw  new NotFoundException("table "+dataCredentials.tableName+" does not exists");
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.TEXT_PLAIN)
+
+    //link to csv : https://dzone.com/articles/how-to-read-a-big-csv-file-with-java-8-and-stream
+    public String loadData(@MultipartForm MultipartBody data){
+        List inputList= new ArrayList<String>();
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(data.file));
+
+            //inputList = br.lines().skip(1).map(mapToItem).collect(Collectors.toList());
+            br.close();
+        }catch (Exception e){
+
+        }
+        return null;
     }
 
     /*
     coordonnée : longitude /lati
     Pair : deux coordonnées
     * */
-
 
     @GET
     @Path("/get")
