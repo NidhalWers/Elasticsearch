75691636243d14c538164fda22d880e392bec32b
NidhalWers <nteyeb75@gmail.com>
2021-03-31 17:29:19 +0200

    ColumnServiceTest & IndexServiceTest not implemented

--- elasticsearch_project/src/test/java/org/snp/unit/service/ColumnServiceTest.java
+++ elasticsearch_project/src/test/java/org/snp/unit/service/ColumnServiceTest.java
@@ -1,4 +1,14 @@
 package org.snp.unit.service;
 
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
 public class ColumnServiceTest {
+
+    @Test
+    public void testGetListColumns(){
+
+    }
+
 }
--- elasticsearch_project/src/test/java/org/snp/unit/service/IndexServiceTest.java
+++ elasticsearch_project/src/test/java/org/snp/unit/service/IndexServiceTest.java
@@ -1,4 +1,11 @@
 package org.snp.unit.service;
 
+import org.junit.jupiter.api.Test;
+
 public class IndexServiceTest {
+
+    @Test
+    public void testCreate(){
+
+    }
 }
