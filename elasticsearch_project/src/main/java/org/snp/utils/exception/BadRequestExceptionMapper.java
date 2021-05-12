bf03e45cff8c93b5b2be05d21f7c541f1684eab9
SinemORUC <sinem.f.oruc@gmail.com>
2021-05-12 17:26:24 +0200

    exception mapper

---
+++ elasticsearch_project/src/main/java/org/snp/utils/exception/BadRequestExceptionMapper.java
@@ -0,0 +1,15 @@
+package org.snp.utils.exception;
+
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class BadRequestExceptionMapper implements ExceptionMapper<BadRequestException> {
+
+    @Override
+    public Response toResponse(BadRequestException e) {
+        return null;
+    }
+}
