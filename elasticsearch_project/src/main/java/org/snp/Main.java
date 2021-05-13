8941d276ff67fd3cab32def704382ed58b60426c
pauliusmi20 <pauliusmick@yahoo.com>
2021-05-13 18:44:46 +0200

    added port

--- elasticsearch_project/src/main/java/org/snp/Main.java
+++ elasticsearch_project/src/main/java/org/snp/Main.java
@@ -1,13 +1,22 @@
 package org.snp;
 
+import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import io.quarkus.runtime.Quarkus;
 
 @QuarkusMain
 public class Main {
+    public static void main(String... args) {
+        Quarkus.run(MyApp.class, args);
+    }
 
-    public static void main(String ... args) {
-        System.out.println("Running main method");
-        Quarkus.run(args);
+    public static class MyApp implements QuarkusApplication {
+
+        @Override
+        public int run(String... args) throws Exception {
+            System.out.println("Do startup logic here");
+            Quarkus.waitForExit();
+            return 0;
+        }
     }
 }
